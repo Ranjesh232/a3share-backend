b5c75b4a62ee91d3e952d0932a92f6ab50c25c98
Ranjesh232 <rsprabhat2000@gmail.com>
2021-08-19 12:30:01 +0530

    Changed to internal css

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
-app.use(express.static('public'));
+app.use(express.static('./public'));
 app.use(express.json());
 
 const connectDB = require('./config/db');
 connectDB();
+
+const corsOption = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+};
+app.use(cors(corsOption));
 
 //Template engine
 app.set('views', path.join(__dirname, '/views'));
